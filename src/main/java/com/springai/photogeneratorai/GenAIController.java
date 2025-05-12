e0ded464b218d22648af4d158fe2d4184bf258c7
shahinp4708 <shahinp4708@gmail.com>
2025-05-12 12:30:55 +0530

    simple chat response

---
+++ src/main/java/com/springai/photogeneratorai/GenAIController.java
@@ -0,0 +1,17 @@
+package com.springai.photogeneratorai;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenAIController {
+    public ChatService chatService;
+    public  GenAIController(ChatService chatService){
+        this.chatService=chatService;
+    }
+    @GetMapping("ask-ai")
+    public  String getResponse(@RequestParam String prompt){
+        return chatService.getResponse(prompt);
+    }
+}

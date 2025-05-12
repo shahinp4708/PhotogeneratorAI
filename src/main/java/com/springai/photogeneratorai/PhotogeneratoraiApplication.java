e62bb1aef2863783f375a64e2657c738d178e58e
shahinp4708 <shahinp4708@gmail.com>
2025-05-12 10:54:32 +0530

    Initial Commit

---
+++ src/main/java/com/springai/photogeneratorai/PhotogeneratoraiApplication.java
@@ -0,0 +1,13 @@
+package com.springai.photogeneratorai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotogeneratoraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotogeneratoraiApplication.class, args);
+	}
+
+}
